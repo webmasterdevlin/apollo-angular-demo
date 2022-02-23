153e7d6a6a4fbfc4c483fc3531fbafcab8acf0f2
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-02-23 18:19:08 +0100

    update

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, Optional, SkipSelf } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { ApolloModule } from "apollo-angular";
+import { HttpLinkModule } from "apollo-angular-link-http";
 
 @NgModule({
   declarations: [],
-  imports: [HttpClientModule],
-  providers: []
+  imports: [HttpClientModule, ApolloModule, HttpLinkModule],
+  providers: [],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
